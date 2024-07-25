bdb64ac9c72ff28225f5bc5ccfc84b184499df13
Darren Vine <darren.vine@googlemail.com>
2024-07-25 23:52:15 +0200

    Revert "feat: keycloak openid userproperty claim protocol mapper"

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/mrparkers/terraform-provider-keycloak/provider"
 )
 
 func main() {
-	var debug bool
-
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
-	opts := &plugin.ServeOpts{
-		Debug:        debug,
-		ProviderAddr: "registry.terraform.io/mrparkers/keycloak",
+	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return provider.KeycloakProvider(nil)
 		},
-	}
-
-	plugin.Serve(opts)
+	})
 }
