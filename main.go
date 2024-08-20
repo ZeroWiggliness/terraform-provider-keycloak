cc77c223c7dda72b21db733a30fb2f6e0e31971b
Darren <darren.vine@goooglemail.com>
2024-08-20 19:11:33 +0200

    Some updates

--- main.go
+++ main.go
@@ -8,18 +8,28 @@ import (
 	"github.com/mrparkers/terraform-provider-keycloak/provider"
 )
 
-func main() {
-	var debug bool
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
 
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	// goreleaser can also pass the specific commit if you want
+	// commit  string = ""
+)
+
+func main() {
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
 	opts := &plugin.ServeOpts{
-		Debug:        debug,
-		ProviderAddr: "registry.terraform.io/mrparkers/keycloak",
-		ProviderFunc: func() *schema.Provider {
-			return provider.KeycloakProvider(nil)
-		},
+		Debug: debugMode,
+
+		// TODO: update this string with the full name of your provider as used in your configs
+		ProviderAddr: "registry.terraform.io/zerowiggliness/keycloak",
+
+		ProviderFunc: provider.New(version),
 	}
 
 	plugin.Serve(opts)
